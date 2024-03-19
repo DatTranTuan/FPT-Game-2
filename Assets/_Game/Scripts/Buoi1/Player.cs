6d2b4a9502608a6aa21ae68bfeadf34dc6e96c49
DatTranTuan <145355463+DatTranTuan@users.noreply.github.com>
2024-03-19 16:35:58 +0700

    Initial commit

---
+++ Assets/_Game/Scripts/Buoi1/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class Player : Character
+{
+    public float maxSpeed;
+    public float jumHeight;
+    Rigidbody2D myBody;
+    Animator myAnim;
+    bool facingRight;
+    bool grounded;
+    public LayerMask groundLayer;
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+        facingRight = true;
+    }
+    void Update()
+    {
+        grounded = CheckGrounded();
+        float move = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(move) > 0.1f)
+        {
+            ChangeAnim("Run");
+        }
+
+        if (Mathf.Abs(move) > 0.1f)
+        {
+            myBody.velocity = new Vector2(move * maxSpeed, myBody.velocity.y);
+
+            // horizontal > 0 -> tra ve 0, neu horizontal <= 0 -> tra ve 180
+            transform.rotation = Quaternion.Euler(new Vector3(0, move > 0 ? 0 : 180, 0));
+            //transform.localScale = new Vector3(horizontal, 1, 1);
+        }
+        else if (grounded)
+        {
+            ChangeAnim("Idle");
+            myBody.velocity = Vector2.zero;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (grounded)
+            {
+                grounded = false;
+                myBody.velocity = new Vector2(myBody.velocity.x, jumHeight);
+            }
+        }
+    }
+    void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+    // void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (other.gameObject.tag == "ground")
+    //     {
+    //         grounded = true;
+    //     }
+    // }
+    private bool CheckGrounded()
+    {
+        Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.25f, Color.red);
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.25f, groundLayer);
+
+        //if (hit.collider != null)
+        //{
+        //    Debug.Log("inside1");
+        //    return true;
+        //}
+        //else
+        //{
+        //    Debug.Log("inside2");
+        //    return false;
+        //}
+
+        return hit.collider != null;
+    }
+
+}
